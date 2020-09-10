a108e4b0efc62fb068a114382008f7bbd8efc12a
Marius Andrian <marius.atj@gmail.com>
2020-09-10 20:20:08 +0800

    trying deployment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -11,7 +11,6 @@ const REACT_APP_SERVER_URL = Endpoints.REACT_APP_SERVER_URL;
 
 const moment = require('moment');
 
-
 export class Comment extends Component {
     constructor(props) {
         super(props)
